f404538ce7a87ae1ed7d1b119a68e6abc2d0e8e2
Artenlaclase <artenlaclase@gmail.com>
2025-01-08 09:34:06 -0300

    servicio de tareas conectado con prisma

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Task } from '@prisma/client';
 
 @Injectable()
 export class TaskService {
+
+  constructor(private prisma: PrismaService) { }
+
+  async getAllTasks(): Promise<Task[]> {
+    return this.prisma.task.findMany();
+  }
+
+  async getTaskById(id: number): Promise<Task> {
+    return this.prisma.task.findUnique({ where: { id } });
+  }
+
+  async createTask(data: Task): Promise<Task> {
+    return this.prisma.task.create({ data });
+  }
+
+  async updateTask(id: number, data: Task): Promise<Task> {
+    return this.prisma.task.update({ where: { id }, data });
+  }
+  async DeleteTask(id: number): Promise<Task> {
+    return this.prisma.task.delete({ where: { id } });
+  }
+
+  /*  estructura creada por nest g res task
   create(createTaskDto: CreateTaskDto) {
     return 'This action adds a new task';
   }
@@ -22,5 +47,5 @@ export class TaskService {
 
   remove(id: number) {
     return `This action removes a #${id} task`;
-  }
+  }*/
 }
